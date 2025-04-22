216299ec2a9613b0e79f340a0b463e3b9f1a5dd5
haseebismail703 <haseebismail703@gmail.com>
2025-04-22 22:33:49 +0500

    fix the bug

--- src/Pages/HomePages/Home.jsx
+++ src/Pages/HomePages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../../Components/HomeComponent/Navbar'
 import HeroSection from '../../Components/HomeComponent/HeroSection'
 import AboutSection from '../../Components/HomeComponent/AboutUs'
 import Services from '../../Components/HomeComponent/Services'
-import QuranSection from '../../Components/HomeComponent/QuranSEction'
+import QuranSection from '../../Components/HomeComponent/QuranSection'
 import Footer from '../../Components/HomeComponent/Footer'
 const Home = () => {
     return (
